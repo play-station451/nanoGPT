fe1f443c7c32caa43ee73ceaa4f9522127d5d318
play-station451 <playstation451discord@gmail.com>
2026-07-25 11:56:34 -0400

    Change model name in chat.py
    Updated the model name to 'BananaMind/BananaMind-2-Medium'.

--- config/chat.py
+++ config/chat.py
@@ -8,7 +8,7 @@ from transformers import (
 )
 from datasets import load_dataset
 
-MODEL_NAME = "SupraLabs/Supra-1.5-50M-Base-exp"
+MODEL_NAME = "BananaMind/BananaMind-2-Medium"
 TEXT_FILES = [
     "https://raw.githubusercontent.com/play-station451/Hdjdjdj/refs/heads/main/input.txt",
     "https://raw.githubusercontent.com/play-station451/Hdjdjdj/refs/heads/main/input2.txt",
