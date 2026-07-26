f93802352f60c6d27830b93f9120a10cc0e3e0b6
play-station451 <playstation451discord@gmail.com>
2026-07-25 23:29:43 -0400

    Update HF_DATASET_NAME for hate speech dataset

--- config/chat.py
+++ config/chat.py
@@ -9,7 +9,7 @@ from transformers import (
 from datasets import load_dataset
 
 MODEL_NAME = "play451/R3tard"
-HF_DATASET_NAME = "Sowmipriya/racism"
+HF_DATASET_NAME = "thefrankhsu/hate_speech_twitter"
 HF_DATASET_SPLIT = "train"
 HF_DATASET_TEXT_COLUMN = "text_content"
 OUTPUT_DIR = "out-supra-chat"
